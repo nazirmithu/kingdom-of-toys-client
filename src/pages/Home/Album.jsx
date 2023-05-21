91cf20042570330dfcdf21caa0fbe7e512ed8dd5
nazirmithu <nazirmithu22@gmail.com>
2023-05-21 19:46:48 +0600

    AOS animation add in photo gallery

--- src/pages/Home/Album.jsx
+++ src/pages/Home/Album.jsx
@@ -5,49 +5,49 @@ const Album = () => {
         <div>
             <h2 className='text-5xl font-bold text-center mt-24 text-purple-600'>Photo Gallery</h2>
             <div className='grid md:grid-cols-2 items-center p-28 space-y-8'>
-                <div className="mockup-phone border-primary w-96 h-96">
+                <div className="mockup-phone border-primary w-96 h-96 " data-aos="fade-right">
                     <div className="camera"></div>
                     <div className="display">
                         <img src="https://i.ibb.co/7VkGDqL/2896369.jpg" alt="" />
                     </div>
                 </div>
-                <div className="mockup-phone border-primary w-96 h-96">
+                <div className="mockup-phone border-primary w-96 h-96 " data-aos="fade-left">
                     <div className="camera"></div>
                     <div className="display">
                         <img src="https://i.ibb.co/jJcS8Bp/2.jpg" alt="" />
                     </div>
                 </div>
-                <div className="mockup-phone border-primary w-96 h-96">
+                <div className="mockup-phone border-primary w-96 h-96" data-aos="fade-up-right">
                     <div className="camera"></div>
                     <div className="display">
                         <img src="https://i.ibb.co/sKzv6Kd/3.png" alt="" />
                     </div>
                 </div>
-                <div className="mockup-phone border-primary w-96 h-96">
+                <div className="mockup-phone border-primary w-96 h-96" data-aos="fade-up-left">
                     <div className="camera"></div>
                     <div className="display">
                         <img src="https://i.ibb.co/zstM2gf/4.png" alt="" />
                     </div>
                 </div>
-                <div className="mockup-phone border-primary w-96 h-96">
+                <div className="mockup-phone border-primary w-96 h-96" data-aos="flip-left">
                     <div className="camera"></div>
                     <div className="display">
                         <img src="https://i.ibb.co/0BMcZ29/5.png" alt="" />
                     </div>
                 </div>
-                <div className="mockup-phone border-primary w-96 h-96">
+                <div className="mockup-phone border-primary w-96 h-96" data-aos="flip-right">
                     <div className="camera"></div>
                     <div className="display">
                         <img src="https://i.ibb.co/CBjYwT0/6.png" alt="" />
                     </div>
                 </div>
-                <div className="mockup-phone border-primary w-96 h-96">
+                <div className="mockup-phone border-primary w-96 h-96 " data-aos="flip-up">
                     <div className="camera"></div>
                     <div className="display">
                         <img src="https://i.ibb.co/Gvhh9QC/7.png" alt="" />
                     </div>
                 </div>
-                <div className="mockup-phone border-primary w-96 h-96">
+                <div className="mockup-phone border-primary w-96 h-96" data-aos="flip-down">
                     <div className="camera"></div>
                     <div className="display">
                         <img src="https://i.ibb.co/hf36khp/8.png" alt="" />
